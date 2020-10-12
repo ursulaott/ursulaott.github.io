b953f13aa5c5f3e480ba79c5262817b4ec19ba31
ursula <uso2000@columbia.edu>
2020-10-11 22:26:40 -1000

    callout

--- script.js
+++ script.js
@@ -16,5 +16,5 @@ var reasons = [
 function newCallout(){
     var un = window.prompt("Enter username: ");
     var rand = Math.floor(Math.random() * (reasons.length));
-    document.getElementById('calloutDisplay').innerHTML = "tumblr user @/"+ un + " is cancelled for " + reasons[rand] + "\n Stay safe. \n";
+    document.getElementById('calloutDisplay').innerHTML = "tumblr user @/"+ un + " is cancelled for " + reasons[rand] + "<br> Stay safe. <br>";
 }

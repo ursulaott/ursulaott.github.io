380b8b1a9030bb734ac8d49b8e86a759ae11a3a7
ursula <uso2000@columbia.edu>
2020-10-11 22:43:19 -1000

    callout

--- script.js
+++ script.js
@@ -5,17 +5,15 @@ var reasons = [
     'uncontrollably sexualing gamers\n',
     'gay\n',
     'owning a discord server\n',
-    'shipping REDACTED\n',
-    'being in the onceler fandom\n'
+    'shipping [REDACTED]\n',
+    'shipping [REDACTED]\n',
+    'shipping [REDACTED]\n',
     'hating children\n',
     'leaving everyone on read\n',
     'shoplifting from walmart\n',
     'kinning bill cipher\n',
     'hating sasuke\n',
     'watching pewdiepie\n',
-    'shipping REDACTED\n',
-    'shipping REDACTED\n',
-    'shipping REDACTED\n',
 ]
 
 
@@ -23,5 +21,5 @@ function newCallout(){
     var un = window.prompt("Enter username: ");
     var rand = Math.floor(Math.random() * (reasons.length));
     document.getElementById('calloutDisplay').innerHTML =
-    "tumblr user @/"+ un +" is cancelled for " + reasons[rand] +"<br><br> Stay safe <br><br><br>";
+    "tumblr user @/"+ un +" is cancelled for " + reasons[rand] +"<br><br> stay safe. <br><br><br>";
 }

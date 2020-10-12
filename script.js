8a1c204cea61c486084da863dbcac9ee0f6f9fc5
ursula <uso2000@columbia.edu>
2020-10-11 22:16:36 -1000

    new

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 var reasons = [
-    'has daddy issues',
-    'simps for incels',
-    'watches anime unironically',
-    'sexualizes gamers uncontrollably',
-    'gay',
-    'owns a discord server'
+    'having daddy issues\n',
+    'simping for incels\n',
+    'watching anime unironically\n',
+    'sexualing gamers uncontrollably\n',
+    'gay\n',
+    'owning a discord server\n'
 ]
 
 
 function newCallout(){
+    var un = window.prompt("Enter username: ");
     var rand = Math.floor(Math.random() * (reasons.length));
-    document.getElementById('calloutDisplay').innerHTML = reasons[rand];
+    document.getElementById('calloutDisplay').innerHTML = "@/"+ un + " is cancelled for " + reasons[rand];
 }
